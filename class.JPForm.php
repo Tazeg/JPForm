f7e405cdf3ec6c008977369849b8437561355901
Tazeg <jeffgazet@gmail.com>
2014-02-12 19:57:36 +0100

    affFreeText, addSelect added + class button

--- class.JPForm.php
+++ class.JPForm.php
@@ -9,40 +9,114 @@
 
 class JPForm
 	{
-	private $_html; // the form html code
-	private $_data; // data sent by form (POST or GET)
-	private $_errors; // error messages from validation rules
-	private $_items; // text, textarea and values (label,name,size,cols,rows...)
+	private $_html; 	// the html code
+	private $_data; 	// data sent by form (POST or GET)
+	private $_errors;	// error messages from validation rules
+	private $_items;	// text, textarea, select, and values (name,size,cols,rows...)
 	
-	// called with i.e. $myform=new JPForm('index.php','POST');
+	//---------------------------------------------------------------------------------------------------------
+	// constructor called with i.e. : $myform=new JPForm('index.php','POST');
+	//---------------------------------------------------------------------------------------------------------
+	
 	public function __construct($action,$method)
 		{
 		$this->_html='<form action="'.$action.'" method="'.$method.'" />'.PHP_EOL;;
 		$this->_errors="";
 		}
 		
-	// add an input type text
-	public function addText($label,$name,$val="",$size=30,$rules) 
+	//---------------------------------------------------------------------------------------------------------
+	// Labels, or any free HTML code you want to add within the HTML code
+	//---------------------------------------------------------------------------------------------------------
+	
+	public function addFreeText($label)
+		{
+		$this->_items[$label]['type']='freeText';
+		$this->_items[$label]['val']=$label;
+		}
+		
+	// return the HTML code for a this free text
+	private static function getHTMLfreeText($carac)
+		{
+		return $carac['val'].PHP_EOL;
+		}
+			
+	//---------------------------------------------------------------------------------------------------------
+	// input type text
+	//---------------------------------------------------------------------------------------------------------
+	
+	public function addText($name,$val="",$size=30,$rules) 
 		{
 		$this->_items[$name]['type']='text';
-		$this->_items[$name]['label']=$label;
 		$this->_items[$name]['name']=$name;
 		$this->_items[$name]['val']=$val;
 		$this->_items[$name]['size']=$size;
-		$this->_items[$name]['rules']=$rules;
+		$this->_items[$name]['rules']=$rules;		
 		}
 		
 	// return the HTML code for a input type text with given caracteristics
 	private static function getHTMLtext($carac)
 		{
-		return $carac['label'].'<br />'.PHP_EOL.'<input type="text" name="'.$carac['name'].'" value="'.$carac['val'].'" size="'.$carac['size'].'" /><br />'.PHP_EOL;
+		return '<input type="text" name="'.$carac['name'].'" value="'.$carac['val'].'" size="'.$carac['size'].'" /><br />'.PHP_EOL;
 		}
 		
-	// add a textarea	
-	function addTextarea($label,$name,$val,$rows,$cols,$rules)
+	//---------------------------------------------------------------------------------------------------------
+	// select
+	//---------------------------------------------------------------------------------------------------------
+	
+	public function addSelect($name,$rules)
+		{
+		$this->_items[$name]['type']='select';
+		$this->_items[$name]['name']=$name;
+		$this->_items[$name]['rules']=$rules;
+		$this->_items[$name]['options']=array();
+		}
+	
+	public function addSelectOption($parent,$val,$label,$selected=false)
+		{
+		while (list($key, $value) = each($this->_items))
+			{
+			if($key!=$parent) {continue;}
+			while (list($key2, $value2) = each($value))
+				{
+				if($key2!='options') {continue;}
+				$this->_items[$key][$key2][]="$val|$label|$selected"; 
+				}
+			reset($value);
+			}	
+		reset($this->_items);	
+		}
+				
+	// return the HTML code for a type select
+	private static function getHTMLselect($carac)
+		{
+		$r='<select name="'.$carac['name'].'">'.PHP_EOL;
+		
+		while (list($key, $value) = each($carac))
+			{
+			if($key!='options') {continue;}
+			while (list($key2, $value2) = each($value))
+				{
+				list($val,$label,$selected)=explode("|",$value2);
+				$r.='<option value="'.$val.'"';
+				if($selected) {$r.=' selected';}
+				$r.='>'.$label.'</option>'.PHP_EOL;
+				}
+			reset($value);
+			}	
+		reset($carac);
+			
+		$r.='</select>'.PHP_EOL;
+				
+		return $r;
+		}
+		
+	//---------------------------------------------------------------------------------------------------------
+	// textarea
+	//---------------------------------------------------------------------------------------------------------
+		 	
+	function addTextarea($name,$val,$rows,$cols,$rules)
 		{
 		$this->_items[$name]['type']='textarea';
-		$this->_items[$name]['label']=$label;
 		$this->_items[$name]['name']=$name;
 		$this->_items[$name]['val']=$val;
 		$this->_items[$name]['rows']=$rows;
@@ -53,24 +127,34 @@ class JPForm
 	// return the HTML code for a textarea with given caracteristics
 	private static function getHTMLtextarea($carac)
 		{
-		return $carac['label'].'<br /><textarea name="'.$carac['name'].'" rows="'.$carac['rows'].'" cols="'.$carac['cols'].'">'.$carac['val'].'</textarea><br />'.PHP_EOL;
+		return '<textarea name="'.$carac['name'].'" rows="'.$carac['rows'].'" cols="'.$carac['cols'].'">'.$carac['val'].'</textarea><br />'.PHP_EOL;
 		}
 		
-	// add a submit button
-	function addBtSubmit($val="Submit",$name="Submit")
+	//---------------------------------------------------------------------------------------------------------
+	// submit button
+	//---------------------------------------------------------------------------------------------------------
+				
+	function addBtSubmit($val="Submit",$name="Submit",$class='')
 		{
 		$this->_items[$name]['type']='submit';
 		$this->_items[$name]['name']=$name;
-		$this->_items[$name]['val']=$val;		
+		$this->_items[$name]['val']=$val;	
+		$this->_items[$name]['class']=$class;
 		}
 	
 	// return the HTML code for a submit button
 	private static function getHTMLsubmit($carac)
 		{
-		return '<input type="submit" name="'.$carac['name'].'" value="'.$carac['val'].'" /><br />'.PHP_EOL;
+		$r='<input type="submit"';
+		if(!empty($carac['class'])) {$r.=' class="'.$carac['class'].'"';}
+		$r.=' name="'.$carac['name'].'" value="'.$carac['val'].'" /><br />'.PHP_EOL;
+		return $r;
 		}		
 		
+	//---------------------------------------------------------------------------------------------------------
 	// return bool if the form has been submitted
+	//---------------------------------------------------------------------------------------------------------
+	
 	public function isSubmitted()
 		{
 		if(count($_POST)>0) {$this->_data=$_POST; return true;}
@@ -78,15 +162,17 @@ class JPForm
 		return false;
 		}
 		
+	//---------------------------------------------------------------------------------------------------------
 	// checking rules for each item
+	// ---------------------------------------------------------------------------------------------------------
+	
 	public function isValid()
 		{
 		$r=true;
 		
-		reset($this->_items);
 		while (list($frmItem, $param) = each($this->_items))
 			{
-			if(!isset($param['rules'])) {continue;} // no rule for submit or anything else
+			if(!isset($param['rules'])) {continue;} // no rule for submit or free text
 		
 			$ex=explode('|',$param['rules']);
 			while (list(, $valueOne) = each($ex))
@@ -129,27 +215,37 @@ class JPForm
 				} // switch
 			} // while
 			
+		reset($this->_items);
 		return $r;
 		}
-		
+
+	//---------------------------------------------------------------------------------------------------------
+	// manage errors
+	//---------------------------------------------------------------------------------------------------------
+	
 	public function getErrors()
 		{
 		return $this->_errors.'<br>';
 		}
-
+	
+	//---------------------------------------------------------------------------------------------------------
 	// display the form
+	//---------------------------------------------------------------------------------------------------------
+		
 	public function render()
-		{
-		reset($this->_items);
+		{		
 		while (list($frmItem, $param) = each($this->_items))
 			{
-			if(isset($this->_data[$frmItem])) {$param['val']=$this->_data[$frmItem];} // to fill form with values submitted
+			if(isset($this->_data[$frmItem])) {$param['val']=$this->_data[$frmItem];} // to fill textarea and text with values submitted
 			$functionname='getHTML'.$param['type'];
 			$this->_html.=self::$functionname($param);
 			}
 		$this->_html.='</form>';
+		reset($this->_items);
 		
 		return $this->_html;
 		}
+		
+	//---------------------------------------------------------------------------------------------------------
 	}
 ?>
--- index.php
+++ index.php
@@ -11,10 +11,41 @@ require 'class.JPForm.php';
 
 // creating form, adding textfields and rules for each (required|email|numeric|integer)
 $myform=new JPForm('index.php','POST');
-$myform->addText('Your name','name','',40,'required');
-$myform->addText('Your mail','email','','','required|email');
-$myform->addTextarea('Your message','message','',10,20,'required');
-$myform->addText('Give me a number !','number','',10,'integer');
+
+$myform->addFreeText('Your name<br>');
+$myform->addText('name','',40,'required');
+
+$myform->addFreeText('Your mail<br>');
+$myform->addText('email','','','required|email');
+
+$myform->addFreeText('Your message<br>');
+$myform->addTextarea('message','',10,20,'required');
+
+$myform->addFreeText('Your birthday<br>');
+$myform->addSelect('day','required|integer');
+for($i=1;$i<=31;$i++) 
+	{
+	$isSelected=false;
+	if(isset($_POST['day'])) {$isSelected=($_POST['day']==$i);}
+	$myform->addSelectOption('day',$i,$i,$isSelected);
+	}
+$myform->addSelect('month','required|integer');
+for($i=1;$i<=12;$i++) 
+	{
+	$isSelected=false;
+	if(isset($_POST['month'])) {$isSelected=($_POST['month']==$i);}		
+	$myform->addSelectOption('month',$i,$i,$isSelected);
+	}
+date_default_timezone_set('Europe/Paris');
+$myform->addSelect('year','required|integer');
+for($i=date("Y");$i>date("Y")-100;$i--)
+	{
+	$isSelected=false;
+	if(isset($_POST['year'])) {$isSelected=($_POST['year']==$i);}		
+	$myform->addSelectOption('year',$i,$i,$isSelected);
+	}
+$myform->addFreeText('<br>');
+
 $myform->addBtSubmit('Envoyer');
 
 // the form has been submitted
@@ -24,7 +55,8 @@ if($myform->isSubmitted())
 	if($myform->isValid())
 		{
 		// values are OK : process what you have to do with $_POST['things']...
-		echo 'Thank you, the form has been sent.<br><br>';
+		echo 'Thank you, the form has been sent.<br>';
+		echo print_r($_POST);
 		}
 	else
 		{
