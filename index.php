e373976b5a6b8f4e8dd48787e6c409cafc7bb0dd
Tazeg <jeffgazet@gmail.com>
2014-03-11 20:12:18 +0100

    spelling mistake on AUTHOR

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <?php
 //-----------------------------------------------------------------------
-//	AUTOR	: Jean-Francois GAZET
-//	WEB 	: http://www.jeffprod.com
+//  AUTHOR	: Jean-Francois GAZET
+//  WEB 	: http://www.jeffprod.com
 //  TWITTER	: @JeffProd
 //  MAIL	: jeffgazet@gmail.com
-//  LICENCE : GNU GENERAL PUBLIC LICENSE Version 2, June 1991
+//  LICENCE 	: GNU GENERAL PUBLIC LICENSE Version 2, June 1991
 //-----------------------------------------------------------------------
 
 require 'class.JPForm.php';
